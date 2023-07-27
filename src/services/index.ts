085d20f04d200fe7c16ccee750894cb0ec792850
Taufik Pragusga <taufik@pragusga.com>
2023-07-27 23:15:16 +0700

    chore: remove detail video service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,4 @@
 export * from './create-comment.service';
-export * from './detail-video.service';
 export * from './list-comments.service';
 export * from './list-products.service';
 export * from './list-videos.service';
