93ebbbc272307591119ca6fe94087457bf6c556a
Taufik Pragusga <taufik@pragusga.com>
2023-07-28 10:41:25 +0700

    feat: throw not found error when video not found

--- src/services/detail-video.service.ts
+++ src/services/detail-video.service.ts
@@ -1,8 +1,11 @@
+import { VideoNotFoundError } from '../errors';
 import { Video } from '../models';
 
 export const detailVideoService = async (id: string) => {
   // Remove the version, comments, and products fields from the response
   const video = await Video.findById(id).select('-version -comments -products');
+
+  if (!video) throw new VideoNotFoundError();
 
   return { data: video };
 };
