d0c860a114635556239ad4955685e3323fa77dae
Taufik Pragusga <taufik@pragusga.com>
2023-07-28 11:08:40 +0700

    feat: add params validation on detail video router

--- src/dtos/index.ts
+++ src/dtos/index.ts
@@ -1,2 +1,3 @@
 export * from './create-comment.body.dto';
 export * from './validate-video-id.params.dto';
+export * from './validate-id.params.dto';
--- src/routers/detail-video.router.ts
+++ src/routers/detail-video.router.ts
@@ -1,15 +1,21 @@
 import { Router } from 'express';
 import { detailVideoService } from '../services';
+import { ValidateIdParamsDto } from '../dtos';
+import { validationParamsMiddleware } from '../shared/middlewares';
 
 const detailVideoRouter = Router();
 
-detailVideoRouter.get('/videos/:id', async (req, res) => {
-  const result = await detailVideoService(req.params.id);
+detailVideoRouter.get(
+  '/videos/:id',
+  validationParamsMiddleware(ValidateIdParamsDto),
+  async (req, res) => {
+    const result = await detailVideoService(req.params.id);
 
-  res.status(200).send({
-    ok: true,
-    ...result,
-  });
-});
+    res.status(200).send({
+      ok: true,
+      ...result,
+    });
+  }
+);
 
 export { detailVideoRouter };
