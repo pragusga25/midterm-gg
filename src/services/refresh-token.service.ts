ce3437a452ce31211590f04b02bbf69c51434291
Taufik Pragusga <taufik@pragusga.com>
2023-08-11 13:19:20 +0700

    fix: return image when refreshing token

--- src/services/refresh-token.service.ts
+++ src/services/refresh-token.service.ts
@@ -1,14 +1,9 @@
 import { JwtUtil } from '../shared/utils';
 
 export const refreshTokenService = async (refreshToken: string) => {
-  const { id, username } = JwtUtil.verifyRefreshToken(refreshToken);
+  const user = JwtUtil.verifyRefreshToken(refreshToken);
 
-  const jwtPayload = {
-    id,
-    username,
-  };
+  const accessToken = JwtUtil.generateAccessToken(user);
 
-  const accessToken = JwtUtil.generateAccessToken(jwtPayload);
-
-  return { accessToken, user: jwtPayload };
+  return { accessToken, user };
 };
