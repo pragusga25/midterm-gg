e3d14a5b684fe2e4baf40d89880dc0989c042e0d
Taufik Pragusga <taufik@pragusga.com>
2023-08-14 08:15:16 +0700

    feat: increase request per seconds

--- src/shared/middlewares/rate-limit.middleware.ts
+++ src/shared/middlewares/rate-limit.middleware.ts
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const rateLimitMiddleware = rateLimit({
   windowMs: 1000, // 1 second
-  max: 5, // Limit each IP to 5 requests per second
+  max: 30, // Limit each IP to 30 requests per second
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: 'Too many requests, please try again later.',
